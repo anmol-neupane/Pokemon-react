e59c0f18676be889273df8f752295090391076f6
Anmol Neupane <56825257+anmol-neupane@users.noreply.github.com>
2025-06-10 11:09:16 +1000

    1 committed

---
+++ src/PokeCard.jsx
@@ -0,0 +1,29 @@
+import PokeDetails from "./PokeDetails";
+
+function PokeCard(props) {
+  console.log(1000, props);
+  //   let name = props.pokemon.name;
+  //   let hp = props.pokemon.hp;
+  //   let ability = props.pokemon.ability;
+
+  let { name, hp, ability, type, attack, defense, image } = props.pokemon;
+  let details = { type, hp, attack, defense };
+
+  return (
+    <div className="card">
+      <div className={"card-front card-" + type.toLowerCase()}>
+        <img src={image} alt="" />
+        <h3>{name}</h3>
+        <PokeDetails details={details} />
+      </div>
+      <div className="card-back">
+        <h1>Ability: {ability.name}</h1>
+        <p>
+          Ability: {ability.name} {ability.desc}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default PokeCard;
---
+++ src/PokeDetails.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+const PokeDetails = (props) => {
+  let { type, hp, attack, defense } = props.details;
+  return (
+    <div className="details">
+      <div>
+        <b>Type:</b> {type}
+      </div>
+      <div>
+        <b>HP:</b> {hp}
+      </div>
+      <div>
+        <b>Attack:</b> {attack}
+      </div>
+      <div>
+        <b>Defense:</b> {defense}
+      </div>
+    </div>
+  );
+};
+
+export default PokeDetails;
